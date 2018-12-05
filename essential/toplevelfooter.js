4fe6b85f512b180af93f43abebdfae6e3e15dc94
Cody Han <hodycan@gmail.com>
2018-12-05 12:40:17 -0800

    init

---
+++ essential/toplevelfooter.js
@@ -0,0 +1,15 @@
+document.write('\
+<div class="footer">\
+	<div id="copyright">\
+		&#169;\
+		<script type="text/javascript">\
+			var d = new Date();\
+			document.write(d.getFullYear());\
+		</script> \
+		WOOF ENTERTAINMENT\
+	</div>\
+	<div>\
+		<a href="mailto:contact@woofentertainment.com">Contact Us</a>\
+	</div>\
+</div>\
+');
